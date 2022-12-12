06c68916ab1ffd84a137e3034f58f4f5e5c7c53d
sheisuka <xxtornex@gmail.com>
2022-12-12 18:07:35 +0300

    add class work: Family Tree

---
+++ class/people.pas
@@ -0,0 +1,39 @@
+﻿program maxMeanHeight;
+    type peopleDataArray = array of array of string;
+    type arrayString = array of string;
+    
+    function getFile(): TextFile;
+    var f: TextFile;
+    begin
+        AssignFile('c:/familytree.txt', f);
+        Result := f;
+    end;
+    
+    function splitString(str: string): arrayString;
+    var 
+        spacesCount: integer := 0;
+        wordsArray : arrayString;
+    begin
+        for var i := 0 to Length(str) do
+        begin
+          if (str[i] = ' ') then
+            
+        end;
+            
+    end;
+    
+    function getPeople(file_: TextFile): peopleDataArray;
+    var 
+      people: peopleDataArray;
+      str: string;
+    begin
+        reset(file_);
+        while not Eof(file_) do
+        begin
+            readln(file_, str);
+            
+        end
+    end;
+begin
+    
+end.
