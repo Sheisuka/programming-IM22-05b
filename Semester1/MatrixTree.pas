392b5a847646e61334e10686103ce89ef9e40b78
sheisuka <xxtornex@gmail.com>
2023-02-08 17:00:37 +0300

    complete first patr of graph task

--- Semester1/MatrixTree.pas
+++ Semester1/MatrixTree.pas
@@ -33,15 +33,9 @@ program MatrixTree;
         randomize;
             // Проверяем запрошены ли четверти с нулями. Если нет - вместо нулей
             // Добавляем в массив пустые строки
-<<<<<<< HEAD
             for var k := 1 to dimension do
                 begin
                     for var i := 1 to dimension do
-=======
-            for k := 1 to dimension do
-                begin
-                    for i := 1 to dimension do
->>>>>>> 023564e2770bdf11b12184f9cba45dfda9f09714
                         begin
                             matrix_element := '   ';
                             if (i <= (dimension / 2) - 1) and (pos('2', quaters) > 0)
@@ -53,15 +47,9 @@ program MatrixTree;
                 end;
             // Проверяем запрошены ли четверти с положительными числами. Если нет - вместо них
             // Добавляем в массив пустые строки
-<<<<<<< HEAD
             for var k := 1 to round(dimension / 2) do
                 begin
                     for var i := 0 + k to dimension + 1 - k do
-=======
-            for k := 1 to round(dimension / 2) do
-                begin
-                    for i := 0 + k to dimension + 1 - k do
->>>>>>> 023564e2770bdf11b12184f9cba45dfda9f09714
                         begin
                             if pos('1', quaters) > 0 then
                                 Str((random(9) + 1), matrix_element)
@@ -74,15 +62,9 @@ program MatrixTree;
                 end;
             // Проверяем запрошены ли четверти с отрицателными числами. Если нет - вместо них
             // Добавляем в массив пустые строки
-<<<<<<< HEAD
             for var k := round(dimension / 2) + 1 to dimension do
                 begin
                     for var i := dimension - k + 1 to k do
-=======
-            for k := round(dimension / 2) + 1 to dimension do
-                begin
-                    for i := dimension - k + 1 to k do
->>>>>>> 023564e2770bdf11b12184f9cba45dfda9f09714
                         begin
                         if pos('3', quaters) > 0 then
                                 Str(-(random(9) + 1), matrix_element)
@@ -98,15 +80,9 @@ program MatrixTree;
     // Процедура для вывода всей матрицы
     procedure print_matrix;
         begin
-<<<<<<< HEAD
             for var k := 1 to dimension do
                 begin
                     for var i := 1 to dimension do
-=======
-            for k := 1 to dimension do
-                begin
-                    for i := 1 to dimension do
->>>>>>> 023564e2770bdf11b12184f9cba45dfda9f09714
                         begin
                             write(matrix_new[k, i]);
                         end;
