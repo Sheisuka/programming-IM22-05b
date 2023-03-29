d7d817cff3499949e65d2490c4e9ef6d4157d30e
sheisuka <xxtornex@gmail.com>
2023-03-29 23:05:06 +0300

    sync

--- Semester2/bigNumbers.pas
+++ Semester2/bigNumbers.pas
@@ -1,4 +1,4 @@
-program bigNumbers;
+﻿program bigNumbers;
 type
     ptr = ^Element;
     Element = record
@@ -8,12 +8,13 @@ type
     end;
 
 var
-    hl1, hr1, hl2, hr2: ptr; 
+    hl1, hr1, hl2, hr2, resL, resR: ptr; 
+    l1, l2: integer;
 
-    procedure createDeck(var headl, headr: ptr);
+    procedure createDeck(var headl, headr: ptr; var len: integer);
     var
         number: integer;
-        cur, dummy: ptr;
+        cur, rhead, lhead: ptr;
     begin
         write('Введите число ---> ');
         readln(number);
@@ -24,26 +25,51 @@ var
             new(lhead);
             lhead^.r := cur;
             cur^.val := number mod 10;
-            number := number div 10
+            number := number div 10;
             cur^.l := lhead;
             cur := lhead;
+            len += 1;
         end;
-        cur^.l^.r := nil;
-        headl := cur^.l;
+        cur^.r^.l := nil;
+        headl := cur^.r;
         headr := rhead;
     end;
 
-    procedure viewFLTR(headl: ptr);
+    procedure viewFRTL(headl: ptr);
     begin
         while (headl <> nil) do
         begin
             write(headl^.val);
-            headl := headl^.r;
+            headl := headl^.l;
         end;
+        writeln;
     end;
+    
+    procedure viewFLTR(headr: ptr);
+    begin
+        while (headr <> nil) do
+        begin
+            write(headr^.val);
+            headr := headr^.r;
+        end;
+        writeln;
+    end;
+    
+    {procedure addNumbers(first, second, resL, resR: ptr);
+    var
+        
+    begin
+      
+    end;}
 
 begin
-    createDeck(hl1, hr1);
+    createDeck(hl1, hr1, l1);
+    writeln('Длина данного числа составляет ', l1);
     viewFLTR(hl1);
-    
-end;
+    viewFRTL(hr1);
+    createDeck(hl2, hr2, l2);
+    writeln('Длина данного числа составляет ', l2);
+    viewFLTR(hl2);
+    viewFRTL(hr2);
+    //addNumbers(hr1, hr2, resL, resR);
+end.
