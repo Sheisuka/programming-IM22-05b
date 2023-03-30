63f1f47d50ea2f9f7bcf23fcc7d3bbedc2160087
sheisuka <xxtornex@gmail.com>
2023-03-30 17:14:01 +0300

    status: done

--- Semester2/bigNumbers.pas
+++ Semester2/bigNumbers.pas
@@ -37,6 +37,7 @@ var
 
     procedure viewFRTL(headl: ptr);
     begin
+        write('Справа налево ');
         while (headl <> nil) do
         begin
             write(headl^.val);
@@ -47,6 +48,7 @@ var
     
     procedure viewFLTR(headr: ptr);
     begin
+        write('Слева направо - ');
         while (headr <> nil) do
         begin
             write(headr^.val);
@@ -55,12 +57,57 @@ var
         writeln;
     end;
     
-    {procedure addNumbers(first, second, resL, resR: ptr);
+    procedure addNumbers(var first, second, resL, resR: ptr; l1, l2: integer);
     var
-        
+        diff, carry, mainL, subL: integer;
+        mainN, subN, newCur: ptr;
     begin
-      
-    end;}
+        if (l1 >= l2) then
+        begin
+            mainN := first;
+            mainL := l1;
+            subL := l2;
+            subN := second;
+        end
+        else
+        begin
+            mainN := second;
+            mainL := l2;
+            subL := l1;
+            subN := first;
+        end;
+        
+        new(resR);
+        newCur := resR;
+        while ((mainN <> nil) and (subN <> nil)) do
+        begin
+            new(resL);
+            resL^.r := newCur;
+            newCur^.val := (carry + mainN^.val + subN^.val) mod 10;
+            carry := (carry + mainN^.val + subN^.val) div 10;
+            newCur^.l := resL;
+            newCur := resL;
+            mainN := mainN^.l;
+            subN := subN^.l;
+        end;
+        while (mainN <> nil) do
+        begin
+            new(resL);
+            resL^.r := newCur;
+            newCur^.val := (carry + mainN^.val) mod 10;
+            carry := (carry + mainN^.val) div 10;
+            newCur^.l := resL;
+            newCur := resL;
+            mainN := mainN^.l;
+        end;
+        if (carry <> 0) then
+            resL^.val := carry
+        else if (resL^.val = 0) then
+        begin
+            resL^.r^.l := nil;
+            resL := resL^.r;
+        end;
+    end;
 
 begin
     createDeck(hl1, hr1, l1);
@@ -71,5 +118,7 @@ begin
     writeln('Длина данного числа составляет ', l2);
     viewFLTR(hl2);
     viewFRTL(hr2);
-    //addNumbers(hr1, hr2, resL, resR);
+    addNumbers(hr1, hr2, resL, resR, l1, l2);
+    viewFLTR(resL);
+    viewFRTL(resR);
 end.
